9d389fbfc4a45ef6fb4a3cc7e0d7378311af1495
Janghoon Lee <chaossky@naver.com>
2018-08-30 19:42:16 +0900

    파이선연습

---
+++ PythonApplication3/PythonApplication3/PythonApplication3.py
@@ -0,0 +1,36 @@
+a=5
+b=2
+
+print (a)
+print (b)
+
+a+=b
+
+print(a)
+print(b)
+
+#print (a+b)
+#print (a-b)
+#print (a*b)
+#print (a/b)
+#print (a//b)
+
+#print (int(5/2))
+#print (5//2)
+#print (5%2)
+#print(5**2)
+#c=divmod(5,2)
+
+#print(c)
+
+#quotient, remainder=divmod(5,2)
+#print(quotient,remainder)
+
+#x=-10
+#print(+x)
+#print(-x)
+
+
+
+
+
